68afa575b674096805bf4aa742892486b2c406a0
Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>
2022-03-21 14:27:37 -0600

    Add buildx multi-arch support
    Signed-off-by: Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>

--- push.sh
+++ push.sh
@@ -5,7 +5,5 @@ export region="eu-west-1"
 export repoName="REPO-NAME-HERE"
 
 aws ecr create-repository --repository-name ${repoName} --profile default --region ${region}
-docker build -t ${repoName}:latest .
 aws ecr get-login-password --region ${region} --profile default | docker login --username AWS --password-stdin ${accountId}.dkr.ecr.${region}.amazonaws.com
-docker tag ${repoName}:latest ${accountId}.dkr.ecr.${region}.amazonaws.com/${repoName}:latest
-docker push ${accountId}.dkr.ecr.${region}.amazonaws.com/${repoName}:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t ${accountId}.dkr.ecr.${region}.amazonaws.com/${repoName}:latest --push .
